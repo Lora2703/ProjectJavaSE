bf5e6fd0bfdc319cdd689fba0688559b4b234e4f
Volodymyr_Hrinchenko <vhrinchenko@dashfinancial.com>
2021-12-07 11:26:44 +0200

    added csv file

---
+++ src/test/java/ua/intita/qa/homework11/Main.java
@@ -0,0 +1,12 @@
+package ua.intita.qa.homework11;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+
+}
+class Base{
+    public void show(int i) {/* realization */}
+}
+class Quest extends Base{ /* code */ }
